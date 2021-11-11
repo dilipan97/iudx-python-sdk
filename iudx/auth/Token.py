b1ac6b2dc503b17b9f58f2cbba49764ece5b31b8
dilipan97 <dilipan@datakaveri.org>
2021-11-11 15:13:13 +0530

    Updated token module

--- iudx/auth/Token.py
+++ iudx/auth/Token.py
@@ -1,8 +1,10 @@
-from requests import Request, Session
 import json
 
+from iudx.common.HTTPEntity import HTTPEntity
+from iudx.common.HTTPResponse import HTTPResponse
 
-class Token():
+
+class Token:
     """
     Provides token functionality for accessing
     single private resources
@@ -10,85 +12,34 @@ class Token():
 
     def __init__(
             self,
-            cert: str = None,
-            key: str = None,
             auth_url: str = "https://authorization.iudx.org.in/auth/v1/token",
-            headers: dict = {"content-type": "application/json"}):
+            body=None,
+            headers=None):
         """
         Token class constructor for requesting tokens
 
-        Args:
-            cert (String): path to the certificate
-            key (String): path to the private key
         """
-        self.cert = cert
-        self.key = key
+        if headers is None:
+            headers = {"content-type": "application/json"}
         self.auth_url = auth_url
+        self.body = body
         self.headers = headers
-        self.token: str = None
-        self.token_response: dict = None
-        self.entities: list = []
-        return
-
-    def add_entity(
-            self,
-            resource_ids: list
-        ):
-        """
-        Method to add a private resource for which a token is required
-
-        Args:
-            resource_id (List): IDs of the private resources to be accessed
-        """
-        self.entities.extend([i for i in resource_ids if i not in self.entities])
-        return
-    
-    def view_entities(self):
-        """
-        Method to view the entitiy ids added
-        """
-        if self.entities:
-            print(self.entities)
-        else:
-            print("No entites added")
-        return
-    
-    def remove_entity(
-            self,
-            resource_ids: list
-        ):
-        """
-        Method to remove added entities
-        
-        Args:
-            resource_ids (List): IDs to be removed
-        """
-        for i in resource_ids:
-            self.entities.remove(i)
+        self.credentials = None
         return
 
     def request_token(self) -> str:
         """
         Method to request a token for the added private resources
 
-        Returns the token (String) to access added the private resources
+        Returns the credentials (access token, expiry, server) to access added the private resources
         """
-        data = {}
-        data["request"] = self.entities
-        
-        s = Session()
-        request = Request('POST',
-                          url=self.auth_url,
-                          data=json.dumps(data),
-                          headers=self.headers
-                          )
-        prepared_req = request.prepare()
 
-        response = s.send(prepared_req,
-                          cert=(self.cert, self.key))
-        if response.status_code == 200:
-            self.token_response = json.loads(response.text)
-            self.token = self.token_response['token']
-            return self.token
+        http_entity = HTTPEntity()
+        response: HTTPResponse = http_entity.post(self.auth_url, json.dumps(self.body), self.headers)
+        result_data = response.get_json()
+
+        if response.get_status_code() == 200:
+            self.credentials = result_data["results"]
+            return self.credentials["accessToken"]
         else:
-            raise RuntimeError(response.text)
+            raise RuntimeError(result_data["detail"])
